a04a03d178d14f255817d548a73ad51742248756
Sharan AJ <sharanajs2001@gmail.com>
2022-05-04 18:52:22 +0530

    server.js configured

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
+const env = require("dotenv");
 
 const app = express();
 
+env.config();
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
+app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.send("hello world!");
+  res.status(200).json({ head: "hello" });
 });
 
 app.use((req, res) => {
   res.status(404).send("Api Not found");
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started!");
 });
