55bdf07519caf21e39f7a24fa2b31d485026ebce
Sharan AJ <sharanajs2001@gmail.com>
2022-05-08 09:22:17 +0530

    .env removed

--- Back-end/server.js
+++ Back-end/server.js
@@ -4,12 +4,21 @@ const morgan = require("morgan");
 const cors = require("cors");
 const env = require("dotenv");
 
+
+// environment variable or const
 env.config();
+
+// mongodb connection cloud
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@multipurpose-trading-db.7up6u.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
+
+// middlewares
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 app.use(cors());
 app.use(express.json());
+
+
 
 app.get("/api", (req, res) => {
   res.status(200).json({ head: "hello" });
